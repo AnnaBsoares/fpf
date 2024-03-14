7170a2aafdaedc0e322a144ed1d54a166eb50c1d
Anna <camargo.anna19@gmail.com>
2024-03-14 17:53:29 -0300

    começando do zero

---
+++ src/test/java/br/com/fempowerfit/fpf/FpfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fempowerfit.fpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
